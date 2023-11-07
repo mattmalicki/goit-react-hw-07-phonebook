6cfe462c1e16659ae1e83cb4ab4764ba28b4eec0
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-07 19:52:54 +0100

    Updating App component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchContacts } from 'redux/operations';
+import { getIsLoading, getError } from 'redux/selectors';
+
+import { Loader } from 'components/atoms/Loader/Loader';
 import { Header } from 'components/atoms/Header/Header';
 import { ContactForm } from 'components/organisms/ContactForm/ContactForm';
 import { ContactList } from 'components/organisms/ContactList/ContactList';
 
 import styles from './App.module.css';
+const dispatch = useDispatch();
+const isLoading = useSelector(getIsLoading);
+const error = useSelector(getError);
+
+useEffect(() => {
+  dispatch(fetchContacts());
+}, [dispatch]);
 
 export const App = () => {
   return (
@@ -14,6 +28,7 @@ export const App = () => {
       <Header level={2} className={styles.appHeader}>
         Contacts
       </Header>
+      {isLoading && !error && <Loader />}
       <ContactList />
     </div>
   );

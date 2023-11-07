5e82e52beb186baad28b041ab6c0ff59ac3277f0
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-07 04:19:32 +0100

    Cloning folders from last homework.

---
+++ src/redux/slices/contactsSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+import { loadStorage, saveStorage } from 'services/localStorage';
+
+const fromLocalStorage = loadStorage('contacts');
+
+const addToStorage = contact => {
+  const fromStorage = loadStorage('contacts');
+  saveStorage('contacts', fromStorage ? [...fromStorage, contact] : [contact]);
+};
+
+const deleteFromStorage = id => {
+  const fromStorage = loadStorage('contacts');
+  saveStorage(
+    'contacts',
+    fromStorage.filter(contact => contact.id !== id)
+  );
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: fromLocalStorage ? fromLocalStorage : [],
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(contact) {
+        const id = nanoid();
+        addToStorage({ ...contact, id });
+        return {
+          payload: {
+            id: id,
+            name: contact.name,
+            number: contact.number,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      deleteFromStorage(action.payload);
+      const index = state.findIndex(contact => contact.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;

6ba1dde483d3acc979b4ea77e9487bb4917af3de
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-07 19:27:15 +0100

    Updating selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,7 @@
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+export const getContacts = state => state.tasks.items;
+
+export const getIsLoading = state => state.tasks.isLoading;
+
+export const getError = state => state.tasks.error;
+
+export const getFilter = state => state.filters.status;

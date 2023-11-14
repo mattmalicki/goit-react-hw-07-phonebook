5b22f57840fef545e734ceb83c0b37c23eb21dfb
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-14 18:02:28 +0100

    Memoized visible contacts

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,9 @@
-export const selectContacts = state => state.contacts.items;
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectContacts = state => {
+  console.log('selectContacts');
+  return state.contacts.items;
+};
 
 export const selectIsLoading = state => state.contacts.isLoading;
 
@@ -6,9 +11,16 @@ export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter.status;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const statusFilter = selectFilter(state);
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, statusFilter) => {
+    console.log('selectVisibleContacts');
 
-  return contacts.filter(obj => obj.name.toLowerCase().includes(statusFilter));
-};
+    return {
+      visible: contacts.filter(obj =>
+        obj.name.toLowerCase().includes(statusFilter)
+      ),
+      inPhonebook: contacts.length,
+    };
+  }
+);

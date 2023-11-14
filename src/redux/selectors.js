3dac1bc86ced42c2560ce5afce561c72e56cab0a
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-14 17:02:12 +0100

    Adding selector for visible contacts

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -5,3 +5,10 @@ export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter.status;
+
+export const visibleContacts = state => {
+  const contacts = selectContacts(state);
+  const statusFilter = selectFilter(state);
+
+  return contacts.filter(obj => obj.name.toLowerCase().includes(statusFilter));
+};

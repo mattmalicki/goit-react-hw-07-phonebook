bd77d3609bf81ba84c1e0e01e134b3080978c520
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-15 17:37:50 +0100

    Adding icons for favourite

--- src/components/molecules/ContactListItem/ContactListItem.jsx
+++ src/components/molecules/ContactListItem/ContactListItem.jsx
@@ -6,23 +6,52 @@ import { Button } from 'components/atoms/Button/Button';
 
 import PropTypes from 'prop-types';
 
+import { deleteIcon, favIcon, noFavIcon } from 'services/icons';
+
 export const ContactListItem = ({ item, classes }) => {
   const dispatch = useDispatch();
 
   const handleDelete = () => {
     dispatch(deleteContact(item.id));
   };
+
+  const handleFav = () => {
+    console.log('Favourite icon');
+  };
   return (
     <li className={classes.contactItem} id={item.id}>
       <Span className={classes.contactNumber}>{item.name}</Span>
       <Span className={classes.contactNumber}>{item.number}</Span>
-      <Button
-        className={classes.contactButton}
-        type="button"
-        onClick={handleDelete}
-      >
-        Delete
-      </Button>
+      <div>
+        <Button
+          className={classes.contactButton}
+          type="button"
+          onClick={handleDelete}
+        >
+          <svg
+            className={classes.iconDelete}
+            width="24px"
+            height="24px"
+            viewBox="0 0 28 28"
+          >
+            <path d={deleteIcon} />
+          </svg>
+        </Button>
+        <Button
+          className={classes.contactButton}
+          type="button"
+          onClick={handleFav}
+        >
+          <svg
+            className={item.favourite ? classes.iconFav : classes.iconNoFav}
+            width="24px"
+            height="24px"
+            viewBox="0 0 28 28"
+          >
+            <path d={item.favourite ? favIcon : noFavIcon} />
+          </svg>
+        </Button>
+      </div>
     </li>
   );
 };

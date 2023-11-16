620818772e26375d005bc3977051e96316b16de9
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-16 07:46:14 +0100

    Now showFilter works!

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -11,12 +11,30 @@ export const selectFilter = state => state.filter.status;
 export const selectShowFilter = state => state.filter.show;
 
 export const selectVisibleContacts = createSelector(
-  [selectContacts, selectFilter],
-  (contacts, statusFilter) => {
+  [selectContacts, selectFilter, selectShowFilter],
+  (contacts, statusFilter, showFilter) => {
+    let visibleContacts;
+    switch (showFilter) {
+      case 'all':
+        visibleContacts = contacts.filter(obj =>
+          obj.name.toLowerCase().includes(statusFilter)
+        );
+        break;
+      case 'favourite':
+        visibleContacts = contacts
+          .filter(obj => obj.favourite)
+          .filter(obj => obj.name.toLowerCase().includes(statusFilter));
+        break;
+      case 'nofavourite':
+        visibleContacts = contacts
+          .filter(obj => !obj.favourite)
+          .filter(obj => obj.name.toLowerCase().includes(statusFilter));
+        break;
+      default:
+        console.log('Error');
+    }
     return {
-      visible: contacts.filter(obj =>
-        obj.name.toLowerCase().includes(statusFilter)
-      ),
+      visible: visibleContacts,
       inPhonebook: contacts.length,
     };
   }
